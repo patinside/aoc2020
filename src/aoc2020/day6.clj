77fcaaa3fb389382802a1d3690d278d20532c465
Romain Patrelle <romain.patrelle@oscaro.com>
2020-12-06 14:40:04 +0100

    day6

---
+++ src/aoc2020/day6.clj
@@ -0,0 +1,35 @@
+(ns aoc2020.day6
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse-in
+  []
+  (str/split (slurp "resources/day6") #"\n\n"))
+
+(defn remove-return-in-groups
+  [groups]
+  (map #(str/replace % #"\n" "") groups))
+
+(defn solution-1
+  []
+  (->> (parse-in)
+       remove-return-in-groups
+       (map #(into #{} %))
+       (map count)
+       (reduce +)))
+
+(defn manage-group
+  [group]
+  (->> group
+       (str/split-lines)
+       (map #(into #{} %))
+       (reduce set/intersection )
+       count))
+
+
+(defn solution-2
+  []
+  (->> (parse-in)
+       (map manage-group)
+       (reduce +)))
+
